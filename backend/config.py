57b3bb57f51e4fcea8188b3c4743c7912ab797b5
Ty4jbt <tyaboe@gmail.com>
2025-06-20 13:19:04 -0700

    updated gitignore

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 
 class DevelopmentConfig:
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Full-Stack-dev97@localhost:3306/mechanics_db'
+    SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:Full-Stack-dev97@localhost:3306/rettnar'
     CACHE_TYPE = 'SimpleCache'
