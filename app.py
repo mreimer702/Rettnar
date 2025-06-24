5e363edacd8dc00f6472e4931a179a4746bea37c
TonySoteloCodeFX <tonysotelo.vfx@gmail.com>
2025-06-24 01:35:55 -0700

    FULL CRUD - USER,ROLE,LOCATION - NEXT(location search filter)

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from app.models import db
 app = create_app('DevelopmentConfig')
 
 with app.app_context():
-    db.drop_all()     # <---------- DROPS ALL TABLES
+    # db.drop_all()     # <---------- DROPS ALL TABLES
     db.create_all()     # <---------- CREATES ALL TABLES
 
 app.run(debug=True)
--- app/blueprints/locations/routes.py
+++ app/blueprints/locations/routes.py
@@ -8,7 +8,7 @@ from typing import List, cast
 
 # xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx LOCATION ROUTES xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 
-@locations_bp.route("/", methods=["POST"])  
+@locations_bp.route("/", methods=["POST"]) # <------------------------------------------ CREATE LOCATION ROUTE
 def create_location():
     try:
         data = request.get_json()
@@ -29,3 +29,45 @@ def create_location():
     db.session.add(new_location)
     db.session.commit()
     return location_schema.jsonify(new_location), 201
+
+@locations_bp.route("/", methods=["GET"]) # <------------------------------------------ GET LOCATIONS ROUTE
+def get_locations():
+    locations = db.session.execute(select(Location)).scalars().all()
+    return locations_schema.jsonify(locations), 200
+
+@locations_bp.route("/<int:location_id>", methods=["GET"]) # <------------------------------------------ GET LOCATION BY ID ROUTE
+def get_location(location_id):
+    location = db.session.execute(select(Location).where(Location.location_id == location_id)).scalars().first()
+    if not location:
+        return jsonify({"error": "Location not found"}), 404
+    return location_schema.jsonify(location), 200
+
+@locations_bp.route("/<int:location_id>", methods=["PUT"]) # <------------------------------------------ UPDATE LOCATION ROUTE
+def update_location(location_id):
+    location = db.session.execute(select(Location).where(Location.location_id == location_id)).scalars().first()
+    if not location:
+        return jsonify({"error": "Location not found"}), 404
+    
+    try:
+        incoming_data = request.get_json()
+        location_schema.load(incoming_data, partial=True)
+    except ValidationError as e:
+        return jsonify(e.messages), 400
+    
+    fields  =["address", "city", "state", "zip_code", "country", "latitude", "longitude"]
+    for field in fields:
+        if field in incoming_data:
+            setattr(location, field, incoming_data[field])
+
+    db.session.commit()
+    return location_schema.jsonify(location), 200
+
+@locations_bp.route("/<int:location_id>", methods=["DELETE"]) # <------------------------------------------ DELETE ROUTE
+def delete_location(location_id):
+    location = db.session.execute(select(Location).where(Location.location_id == location_id)).scalars().first()
+    if not location:
+        return jsonify({"error": "Location not found"}), 404
+    
+    db.session.delete(location)
+    db.session.commit()
+    return jsonify({"message": "Location deleted successfully"}), 200
